d5410a093cf6bdf33ea630bb59f542975a2a5303
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2021-09-10 14:31:25 +0530

    Create Project Euler #2: Even Fibonacci numbers.java
    This problem is a programming version of Problem 2 from projecteuler.net
    
    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with  and , the first  terms will be:
    
    By considering the terms in the Fibonacci sequence whose values do not exceed , find the sum of the even-valued terms.
    
    Input Format
    
    First line contains  that denotes the number of test cases. This is followed by  lines, each containing an integer, .
    
    Constraints
    
    Output Format
    
    Print the required answer for each test case.
    
    Sample Input 0
    
    2
    10
    100
    Sample Output 0
    
    10
    44

---
+++ Project Euler #2: Even Fibonacci numbers.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int x = 0; x < t; x++){
+            long n = in.nextLong();
+            long a=0,b=1,s=0,c=0;
+            while((a+b)<=n)
+            {
+                c=a+b;
+                a=b;
+                b=c;
+                if(c%2==0)
+                {
+                    s=s+c;
+                }
+            }
+            System.out.println(s);
+        }
+    }
+}
